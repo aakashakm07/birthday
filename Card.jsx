ec37b0ac6a8457eac640637af1380f68acfc6190
aakashakm07 <157091779+aakashakm07@users.noreply.github.com>
2024-08-01 21:19:55 +0530

    Add files via upload
    first

---
+++ Card.jsx
@@ -0,0 +1,31 @@
+export default () => (
+    <div className="birthdayCard">
+      <div className="cardFront">
+        <div className="container">
+        <p className="text">HAPPY BIRTHDAY ADITYA!</p>
+        </div>
+        <div className="balloons">
+          <div className="balloonOne" />
+          <div className="balloonTwo" />
+          <div className="balloonThree" />
+          <div className="balloonFour" />
+        </div>
+      </div>
+      <div className="cardInside">
+        <h3 className="back">HAPPY BIRTHDAY ADITYA!</h3>
+        <p>Dear MamA,</p>
+        <p>
+          Happy birthday!! I hope your day is filled with lots of love and
+          laughter! May all of your birthday wishes come true.
+        </p>
+        <p className="name">A<sup>4</sup></p>hello
+      </div>
+      <div >
+       <spn className="image"> <img className="add" src="./"></img></spn>
+        <img className="add" src="./2.jpeg"></img>
+        <img className="add" src="./"></img>
+        <img className="add" src="./"></img>
+      </div>
+    </div>
+  );
+  
